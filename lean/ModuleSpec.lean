be6eae1ecfc5798ebf96e0a9cf8cd5ce64c8065f
David Binder <david.binder@uni-tuebingen.de>
2026-06-06 17:59:04 +0200

    Add Imports Exports

--- lean/ModuleSpec.lean
+++ lean/ModuleSpec.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `ModuleSpec` library.
 -- Import modules here that should be built as part of the library.
-import ModuleSpec.Basic
+import ModuleSpec.Imports
+import ModuleSpec.Exports
