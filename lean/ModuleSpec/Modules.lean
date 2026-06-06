3c4aa8337aab4042626836e15e3368ae03dfff90
Isaac van Bakel <isaac.vanbakel@inf.ethz.ch>
2026-06-06 18:18:30 +0200

    Export list datatypes

--- lean/ModuleSpec/Modules.lean
+++ lean/ModuleSpec/Modules.lean
@@ -1,1 +1,13 @@
 -- Syntax of Modules
+import ModuleSpec.Names
+
+inductive SubSpec where
+  | AllSubs
+  | Subs : List Name → SubSpec
+
+inductive EntExp where
+  | Ent : QName → Option (SubSpec) → EntExp
+
+inductive ExpListEntry where
+  | EntExp : EntSpec → ExpListEntry
+  | ModuleExp : ModName → ExpListEntry
