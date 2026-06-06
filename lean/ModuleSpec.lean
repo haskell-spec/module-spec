1c6b963ad85bffdbc0a1ac77c3cf44364f9d96bd
David Binder <david.binder@uni-tuebingen.de>
2026-06-06 18:04:50 +0200

    Define Names

--- lean/ModuleSpec.lean
+++ lean/ModuleSpec.lean
@@ -1,4 +1,3 @@
--- This module serves as the root of the `ModuleSpec` library.
--- Import modules here that should be built as part of the library.
 import ModuleSpec.Imports
 import ModuleSpec.Exports
+import ModuleSpec.Names
---
+++ lean/ModuleSpec/Names.lean
@@ -0,0 +1,46 @@
+inductive Name where
+  | MkName : String → Name
+  deriving Ord, BEq
+
+inductive ModName where
+  | ModName : String → ModName
+  deriving Ord, BEq
+
+inductive QName where
+  | Qualified : ModName → Name → QName
+  | UnQualified : Name → QName
+  deriving Ord, BEq
+
+
+def getQualifier (q : QName) : Option ModName :=
+  match q with
+  | QName.Qualified m _ => some m
+  | QName.UnQualified _ => none
+
+def getQualified(q : QName) : Name :=
+  match q with
+  | QName.Qualified _ n => n
+  | QName.UnQualified n => n
+
+def mkUnqual(n : Name) : QName :=
+  QName.UnQualified n
+
+def mkQual(m : ModName)(n : Name) : QName :=
+  QName.Qualified m n
+
+def isQual(q : QName) : Bool :=
+  match q with
+  | QName.Qualified _ _ => true
+  | QName.UnQualified _ => false
+
+def qual(m : ModName)(q : QName) : QName :=
+  mkQual m (getQualified q)
+
+class ToSimple (α : Type) where
+  toSimple : α → Name
+
+instance ToSimpleName : ToSimple Name where
+  toSimple x := x
+
+instance ToSimpleQName : ToSimple QName where
+  toSimple x := getQualified x
